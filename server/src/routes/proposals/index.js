4dca6fe0fa36fa6b8bc6eda690500cc73e0b8087
mahisurrahman <zordanblog009@gmail.com>
2024-04-22 10:45:51 +0600

    New branch

--- server/src/routes/proposals/index.js
+++ server/src/routes/proposals/index.js
@@ -5,9 +5,7 @@ const proposalController = require("../../controllers/proposals/index.js");
 const multerMiddleware = require("../../middlewares/multer.middleware.js");
 
 router.post("/crt", multerMiddleware.single("file"), proposalController.createProposals);
-router.get("/get", proposalController.getProposals);
-router.post("/update/:id", proposalController.updateProposal);
-router.post("/updatebyhod/:id", proposalController.updateByHod);
+router.post("/get", proposalController.getProposals);
 // router.post("/get/single", proposalController.getSingleProposals)
 // router.get("/getAll", blogController.getAll);
 // router.get("/getSingle/:id", blogController.getSingleBlog);
