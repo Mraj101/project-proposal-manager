4d0562105331d9888fc868e7ae2e96e31e4e4f9c
Hasnain <mrajhasnain101@gmail.com>
2024-04-22 10:59:47 +0600

    hodpanel

--- client/src/components/HodPanel.js
+++ client/src/components/HodPanel.js
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import axios from "axios";
-const HodPanel = () => {
 
+const HodPanel = () => {
   const [proposals, setProposals] = useState([]);
   const { usr, setUsr } = useAuthContext();
-  const [loading, setLoading] = useState(true);
 
-  const fetchProposals = async (receivedUsr) => {
+  const fetchProposals = async () => {
     try {
       const response = await axios.get(
         "http://localhost:8000/api/v1/proposals/get"
@@ -33,7 +32,9 @@ const HodPanel = () => {
     } catch (error) {
       console.error("Error accepting proposal:", error);
     }
-  };
+  }; 
+  
+
 
   useEffect(() => {
     const userDataFromStorage = JSON.parse(localStorage.getItem("user"));
@@ -43,27 +44,25 @@ const HodPanel = () => {
   }, []);
 
   useEffect(() => {
-    fetchProposals(usr);
-  }, [usr]);
+    fetchProposals();
+  }, []);
 
   return (
-    <div className="">
-            <p>Hod panel</p>
-          <div className="w-full relative">
-            <img
-              src="https://media.istockphoto.com/id/1423164674/photo/african-american-female-teacher-in-school-hallway.webp?b=1&s=170667a&w=0&k=20&c=wTc3pp3Aq470DdWJT9pFpWEFkWfjxXCYO7ITfQ6y37I="
-              className="w-full h-[400px] object-cover"
-              alt="Not Found"
-            />
-            <div className="absolute inset-0 bg-black opacity-50 "></div>
-            <h1 className="text-center text-8xl font-extrabold text-slate-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-2 rounded-lg border-slate-100 px-4 py-2">
-              Dept Head 
-            </h1>
-            <div className="absolute inset-0 bg-black opacity-30 "></div>
-          </div>
+    <div>
+      <div className="w-full relative">
+        <img
+          src="https://media.istockphoto.com/id/1423164674/photo/african-american-female-teacher-in-school-hallway.webp?b=1&s=170667a&w=0&k=20&c=wTc3pp3Aq470DdWJT9pFpWEFkWfjxXCYO7ITfQ6y37I="
+          className="w-full h-[400px] object-cover"
+          alt="Not Found"
+        />
+        <div className="absolute inset-0 bg-black opacity-50 "></div>
+        <h1 className="text-center text-8xl font-extrabold text-slate-100 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-2 rounded-lg border-slate-100 px-4 py-2">
+          Dept Head
+        </h1>
+        <div className="absolute inset-0 bg-black opacity-30 "></div>
+      </div>
 
-
-          <div className="overflow-x-auto mx-10 my-10 border-2 rounded-lg">
+      <div className="overflow-x-auto mx-10 my-10 border-2 rounded-lg">
         <table className="table">
           <thead className="border-b-2">
             <tr>
@@ -83,7 +82,13 @@ const HodPanel = () => {
                 <td>{proposal.description}</td>
                 <td>{proposal.file.substring(0, 10)}</td>
                 <td>{proposal.createdAt.split('T')[0]}</td>
-                <td colSpan={proposal.isAccepted ? "2" : "1"}>
+                <td>
+                  {(proposal.isAccepted  &&
+                    proposal.isAccepetedByHOD) ? 
+                      (<span className="px-4 py-2 rounded-lg bg-green-400 text-white">
+                        Approved
+                      </span>
+                  ) : (
                     <div className="flex gap-5">
                       <button
                         className="flex items-center gap-2 px-4 py-2 rounded-lg bg-green-400 text-white"
@@ -91,10 +96,11 @@ const HodPanel = () => {
                       >
                         ✔️ Accept
                       </button>
-                      <button className="flex items-center gap-2 px-4 py-2 rounded-lg bg-red-400 text-white">
+                      <button  className="flex items-center gap-2 px-4 py-2 rounded-lg bg-red-400 text-white">
                         ❌ Reject
                       </button>
                     </div>
+                  )}
                 </td>
               </tr>
             ))}
